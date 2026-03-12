af44eb7590b8735a596d2806d553d76c2b398e69
Arman Singh <143929792+Arman0604@users.noreply.github.com>
2026-03-12 18:56:48 +0530

    Remove main function from bfs.cpp
    Removed main function and related graph initialization code.

--- Graphs Algorithms/bfs.cpp
+++ Graphs Algorithms/bfs.cpp
@@ -34,38 +34,3 @@ void bfs(int start, vector<vector<int>> &adj, vector<bool> &visited) {
     }
 }
 
-int main() {
-
-    int n = 5; // number of vertices
-    int m = 5; // number of edges
-
-    // adjacency list for graph
-    vector<vector<int>> adj(n);
-
-    // edges of undirected graph
-    vector<pair<int,int>> edges = {
-        {0,1},
-        {0,2},
-        {1,3},
-        {2,3},
-        {3,4}
-    };
-
-    // build adjacency list
-    for(auto edge : edges) {
-        int u = edge.first;
-        int v = edge.second;
-
-        adj[u].push_back(v);
-        adj[v].push_back(u); // because graph is undirected
-    }
-
-    vector<bool> visited(n, false);
-
-    cout << "BFS Traversal: ";
-
-    // start BFS from node 0
-    bfs(0, adj, visited);
-
-    return 0;
-}
