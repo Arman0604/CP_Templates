c2c92548e73d8cf8d0cdc36426b209a652c9f0c6
Arman Singh <143929792+Arman0604@users.noreply.github.com>
2026-03-12 19:29:09 +0530

    Add advanced.cpp with fast IO and debug template

---
+++ Template cpp/advanced.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ================= Fast IO =================
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+// ================= Debug Template =================
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int x) { cerr << x; }
+void _print(long long x) { cerr << x; }
+void _print(string x) { cerr << x; }
+void _print(char x) { cerr << x; }
+void _print(double x) { cerr << x; }
+
+template <class T, class V>
+void _print(pair<T,V> p){
+    cerr << "{";
+    _print(p.first);
+    cerr << ", ";
+    _print(p.second);
+    cerr << "}";
+}
+
+template <class T>
+void _print(vector<T> v){
+    cerr << "[ ";
+    for(auto i : v){
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T>
+void _print(set<T> s){
+    cerr << "{ ";
+    for(auto i : s){
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+template <class T, class V>
+void _print(map<T,V> m){
+    cerr << "{ ";
+    for(auto i : m){
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+// ================= Main =================
+int main() {
+
+    fastio;
+
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin >> a[i];
+
+    debug(n);
+    debug(a);
+
+    cout << "Program executed\n";
+
+    return 0;
+}
